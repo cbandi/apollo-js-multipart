3d7e17cd73b3fcb92bd6229829e0c48c02cd833c
cbandi <cbandi@IM-LP-442.local>
2020-09-01 13:04:25 +0530

    Changes form terminal

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { ApolloLink, Observable } from 'apollo-link';
 import { createUploadLink } from 'apollo-upload-client';
 import { BatchHttpLink } from 'apollo-link-batch-http';
 import options from 'options';
-///changes
+///changes - form terminal
 
 const cache = new InMemoryCache({
   cacheRedirects: {}
